7017712121026ed127da05be3271f2245a997c67
ChundruManohar <145706366+ChundruManohar@users.noreply.github.com>
2023-09-26 10:37:05 +0530

    Update 5Q.py

---
+++ 5Q.py
@@ -0,0 +1,8 @@
+#user for their password. If the password is "password123,"
+# print "Access granted." Otherwise, print "Access denied."
+
+user = input("Enter a password: ")
+if user == 'password123':
+    print('Access Granted')
+else:
+    print('Access denied')
