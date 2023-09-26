181a5686bfbf2e67cccc57451725efc30b8a073f
ChundruManohar <145706366+ChundruManohar@users.noreply.github.com>
2023-09-26 10:34:24 +0530

    Update 1Q.py

---
+++ 1Q.py
@@ -0,0 +1,6 @@
+#integer input print even if its even & odd if an odd number
+a = int(input('Enter a number '))
+if (a%2) == 0:
+    print('Its is a even number: ',a)
+else:
+    print('Its is a odd number: ',a)
